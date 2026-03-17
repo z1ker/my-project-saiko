1eba90f4a81b1d25a04afc58ca47d5c84a26ba34
Admin <kiramuryst@gmail.com>
2026-03-17 21:39:36 +0200

    init project structure

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from utils import add
 
 def main():
-    a = 2
-    b = 3
-    print("Result:", add(a, b))
+    print(add(2, 3))
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,8 +1,2 @@
 def add(a, b):
     return a + b
-
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Division by zero")
-    return a / b
